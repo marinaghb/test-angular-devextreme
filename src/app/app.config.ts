7312fa0b57885678ea9b7e7b5003fd3035ff45d5
mtokareva <mtokareva@cgames.team>
2025-10-16 11:46:42 +0700

    add devextreme to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { routes } from './app.routes';
+import {
+    AppInfoService,
+    AuthGuardService,
+    AuthService,
+    ScreenService,
+} from './shared/services';
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withHashLocation()),
+    AuthGuardService,
+    AuthService,
+    ScreenService,
+    AppInfoService,
+  ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+import themes from 'devextreme/ui/themes';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+themes.initialized(() => {
+  bootstrapApplication(AppComponent, appConfig)
+    .catch((err) => console.error(err));
+});
